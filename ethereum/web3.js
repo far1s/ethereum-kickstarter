0c8f6c2abd6434e20c577fe3cc63bfbf1b48e1eb
Faris Brni <farisbrni@gmail.com>
2018-03-28 17:37:58 +0200

    Adjust web3 injection to work on server

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -5,6 +5,22 @@ import Web3 from 'web3';
  * provider from metamask. Here we make the assumption
  * that metamask has already injected web3
 */
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+if (
+    // check if we're in the browser
+    typeof window !== 'undefined' &&
+    // check if metamask has already injected web3
+    typeof window.web3 !== 'undefined'
+) {
+    // we're in the browser and metamask is running
+    web3 = new Web3(window.web3.currentProvider);
+} else {
+    // we're in the server and the user is not running metamask
+    const provider = new Web3.providers.HttpProvider(
+        'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q'
+    );
+    web3 = new Web3(provider);
+}
 
 export default web3;
