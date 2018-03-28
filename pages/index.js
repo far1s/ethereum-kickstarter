d5e2d62ebefe2caf16eabfe6603fdb77c6739beb
Faris Brni <farisbrni@gmail.com>
2018-03-28 18:01:28 +0200

    Add server side lifecycle function provided by next.js

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import factory from '../ethereum/factory';
 
 export default class CampaignIndex extends React.Component {
-    async componentDidMount() {
-        const campaign = await factory.methods.getDeployedCampaigns().call();
-        console.log('campaign: ', campaign);
+    /**
+     * Unsing keyword "static" it defines a class function.
+     * Is not assigned to instances of the class. If we want
+     * to call this function, we don't have to create an
+     * instance. Next requires this function.
+    */
+    static async getInitialProps() {
+        const campaings = await factory.methods.getDeployedCampaigns().call();
+        return {
+            campaings
+        }
     }
 
     render() {
-        return <div>Index</div>;
+        return <div>{this.props.campaings[0]}</div>;
     }
 }
