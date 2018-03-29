20cc59764dd400f1b0a1debb9feb7e7f7a763926
Faris Brni <farisbrni@gmail.com>
2018-03-29 11:02:31 +0200

    Add dynamic route component

---
+++ pages/campaigns/show.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+export default class CampaignShow extends React.Component {
+    render() {
+        return (
+            <Layout>
+                Test
+            </Layout>
+        );
+    }
+}
